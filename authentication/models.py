cad0514d1161d6c034e93e2183d6fc2eeccc4ff8
nunolemos <nunolemos@MSI-Kali.Home>
2022-04-24 23:29:59 +0100

    font_weight and font_style fields added to FontSettings model

--- authentication/models.py
+++ authentication/models.py
@@ -23,10 +23,30 @@ class Profile(models.Model):
 
 
 class FontSettings(models.Model):
+
+    FONT_WEIGHT_CHOICES = [
+        ("NORMAL", "normal"),
+        ("BOLD", "bold")
+    ]
+
+    FONT_STYLE_CHOICES = [
+        ("NORMAL", "normal"),
+        ("ITALIC", "italic")
+    ]
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     font_family = models.TextField(default="Roboto-Regular")
     font_size = models.IntegerField(default=14)
     font_color = models.TextField(default="#7F7F7F")
+    font_weight = models.CharField(max_length=6, choices=FONT_WEIGHT_CHOICES, default="NORMAL")
+    font_style = models.CharField(max_length=6, choices=FONT_STYLE_CHOICES, default="NORMAL")
 
     def serialize(self):
-        return {"fontFamily": self.font_family, "fontSize": self.font_size, "lineHeight": self.font_size, "color": self.font_color}
+        return {
+            "fontFamily": self.font_family,
+            "fontSize": self.font_size,
+            "lineHeight": self.font_size,
+            "color": self.font_color,
+            "fontWeight": self.font_weight,
+            "fontStyle": self.font_style
+        }
